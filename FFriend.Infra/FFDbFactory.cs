93a01481f71c9d2bf91c0e8fffa0b961393e51e5
Alceu-wv <alceu.wv@gmail.com>
2023-03-23 07:31:06 -0300

    DB Factory

---
+++ FFriend.Infra/FFDbFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FFriend.Infra
+{
+    public class FFDbFactory : IDesignTimeDbContextFactory<FFDbContext>
+    {
+        public FFDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FFDbContext>();
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=FFriendDb;Trusted_Connection=True;MultipleActiveResultSets=true"
+                );
+            return new FFDbContext(optionsBuilder.Options);
+        }
+    }
+}
